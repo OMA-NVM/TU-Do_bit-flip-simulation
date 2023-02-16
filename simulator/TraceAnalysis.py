72a543ef5a35ed5830e7ee72e3deb70d7cf3e7de
Duy <minhduy.truong@tu-dortmund.de>
2023-02-16 15:47:51 +0000

    More user inputs to customize Traces more/modified script

--- simulator/TraceAnalysis.py
+++ simulator/TraceAnalysis.py
@@ -25,6 +25,10 @@ parser.add_argument('--f', type=str, required=True,
                     help="Folder containing NVMain simulation results.")
 parser.add_argument('--m', type=str, required=False, default="all",
                     help="Memmory region (text|ro|data|bss|heap|stack|all) to analyse, defaults to 'all'.")
+parser.add_argument('--w', type=str, required=False, default="64",
+                    help="Window size to calculate AEW (Input: 8|16|32|64|all), defaults to '64'.")
+parser.add_argument('--p', type=bool, required=False, default=False,
+                    help="Turn on/off plotting, defaults to plot AEW of '64'.")
 # Parse the argument
 args = parser.parse_args()
 print(args)
@@ -225,6 +229,8 @@ end = np.size(bits)
 # =====Calculate Metrics===============
 # =====================================
 
+#Calculating AEW with given start/endindex and windowsize
+#IMPORTANT: The algorithm searches for the minimum value but the values are named MAX through out the script! (not very smart)
 def calculateAEW(startIndex, endIndex, windowSize):
     leftBound = 0
     rightBound = 0
@@ -238,6 +244,7 @@ def calculateAEW(startIndex, endIndex, windowSize):
                 leftBound = i
                 rightBound = i+windowSize
                 count = 0
+            #Counts if there are multiple maxAEW for the trace (might be interesting)
             elif aeWindow == maxWindowValue:
                 count+1
         else:
@@ -257,7 +264,7 @@ timetaken = time.time() - startTime
 print("Calculating took: " + str(int(timetaken)) + " seconds")
 
 
-#filtering memory index for calculation
+#Filtering memory index for calculation
 segment = args.m
 print("Chosen segment: " + segment)
 def memory_region():
@@ -270,14 +277,10 @@ def memory_region():
             return dataStartIndex[0][0], dataEndIndex[0][0]
         case "bss":
             return bssStartIndex[0][0], bssEndIndex[0][0]
-        # case "heap":
-        #     return heapStartIndex[0][0], heapEndIndex[0][0]
-        # case "stack":
-        #     return stackStartIndex[0][0], stackEndIndex[0][0]
         case "heap":
-            return stackEndIndex[0][0], end
+             return heapStartIndex[0][0], heapEndIndex[0][0]
         case "stack":
-            return stackEndIndex[0][0], end      
+             return stackStartIndex[0][0], end    
         case "all":
             return textStartIndex[0][0], end
 
@@ -286,53 +289,92 @@ startindex, endindex = memory_region()
 # =====Print Metrics===========
 # =============================
 
-#calculating AE and all AEW
+#Calculating stuff like AE and AEW in all sizes
 
 achievedEndurance = (np.sum(flips[startindex:endindex])/np.shape(flips)[0]) / np.max(flips[startindex:endindex])
-aewMax8, leftindex8, rightindex8 = calculateAEW(startindex,endindex,7)
-print("FOR N=8 and " + segment + " segment!")
-print("AE: " + str(achievedEndurance))
-print("AEW: " + str(aewMax8))
-print("AE/AEW " + str(achievedEndurance/aewMax8))
-print("AEW/AE " + str(aewMax8/achievedEndurance))
-print("left and right " + str(leftindex8), str(rightindex8))
-print("\n\n")
 
-aewMax16, leftindex16, rightindex16 = calculateAEW(startindex,endindex,15)
-print("FOR N=16 and " + segment + " segment!")
-print("AE: " + str(achievedEndurance))
-print("AEW: " + str(aewMax16))
-print("AE/AEW " + str(achievedEndurance/aewMax16))
-print("AEW/AE " + str(aewMax16/achievedEndurance))
-print("left and right " + str(leftindex16), str(rightindex16))
-print("\n\n")
+print("Chosen size for AEW: " + args.w + "\n\n")
 
-aewMax32, leftindex32, rightindex32 = calculateAEW(startindex,endindex,31)
-print("FOR N=32 and " + segment + " segment!")
-print("AE: " + str(achievedEndurance))
-print("AEW: " + str(aewMax32))
-print("AE/AEW " + str(achievedEndurance/aewMax32))
-print("AEW/AE " + str(aewMax32/achievedEndurance))
-print("left and right " + str(leftindex32), str(rightindex32))
-print("\n\n")
+#Filtering for --w input to calculate wanted AEW
 
+if args.w == "8":
+    aewMax8, leftindex8, rightindex8 = calculateAEW(startindex,endindex,7)
+    print("FOR N=8 and " + segment + " segment!")
+    print("AE: " + str(achievedEndurance))
+    print("AEW: " + str(aewMax8))
+    print("AE/AEW " + str(achievedEndurance/aewMax8))
+    print("AEW/AE " + str(aewMax8/achievedEndurance))
+    print("left and right " + str(leftindex8), str(rightindex8))
+    print("\n\n")
 
-aewMax64, leftindex64, rightindex64 = calculateAEW(startindex,endindex,63)
-print("FOR N=64 and " + segment + " segment!")
-print("AE: " + str(achievedEndurance))
-print("AEW: " + str(aewMax64))
-print("AE/AEW " + str(achievedEndurance/aewMax64))
-print("AEW/AE " + str(aewMax64/achievedEndurance))
-print("left and right " + str(leftindex64), str(rightindex64))
-print("\n\n")
+elif args.w == "16":
+    aewMax16, leftindex16, rightindex16 = calculateAEW(startindex,endindex,15)
+    print("FOR N=16 and " + segment + " segment!")
+    print("AE: " + str(achievedEndurance))
+    print("AEW: " + str(aewMax16))
+    print("AE/AEW " + str(achievedEndurance/aewMax16))
+    print("AEW/AE " + str(aewMax16/achievedEndurance))
+    print("left and right " + str(leftindex16), str(rightindex16))
+    print("\n\n")
+elif args.w == "32":
+    aewMax32, leftindex32, rightindex32 = calculateAEW(startindex,endindex,31)
+    print("FOR N=32 and " + segment + " segment!")
+    print("AE: " + str(achievedEndurance))
+    print("AEW: " + str(aewMax32))
+    print("AE/AEW " + str(achievedEndurance/aewMax32))
+    print("AEW/AE " + str(aewMax32/achievedEndurance))
+    print("left and right " + str(leftindex32), str(rightindex32))
+    print("\n\n")
 
+elif args.w == "all":
+    print("AE: " + str(achievedEndurance))
+    aewMax8, leftindex8, rightindex8 = calculateAEW(startindex,endindex,7)
+    print("FOR N=8 and " + segment + " segment!")
+    print("AEW: " + str(aewMax8))
+    print("AE/AEW " + str(achievedEndurance/aewMax8))
+    print("AEW/AE " + str(aewMax8/achievedEndurance))
+    print("left and right " + str(leftindex8), str(rightindex8))
+    print("\n\n")
+    aewMax16, leftindex16, rightindex16 = calculateAEW(startindex,endindex,15)
+    print("FOR N=16 and " + segment + " segment!")
+    print("AEW: " + str(aewMax16))
+    print("AE/AEW " + str(achievedEndurance/aewMax16))
+    print("AEW/AE " + str(aewMax16/achievedEndurance))
+    print("left and right " + str(leftindex16), str(rightindex16))
+    print("\n\n")
+    aewMax32, leftindex32, rightindex32 = calculateAEW(startindex,endindex,31)
+    print("FOR N=32 and " + segment + " segment!")
+    print("AEW: " + str(aewMax32))
+    print("AE/AEW " + str(achievedEndurance/aewMax32))
+    print("AEW/AE " + str(aewMax32/achievedEndurance))
+    print("left and right " + str(leftindex32), str(rightindex32))
+    print("\n\n")
+    aewMax64, leftindex64, rightindex64 = calculateAEW(startindex,endindex,63)
+    print("FOR N=64 and " + segment + " segment!")
+    print("AEW: " + str(aewMax64))
+    print("AE/AEW " + str(achievedEndurance/aewMax64))
+    print("AEW/AE " + str(aewMax64/achievedEndurance))
+    print("left and right " + str(leftindex64), str(rightindex64))
+    print("\n\n")
+
+else:
+    aewMax64, leftindex64, rightindex64 = calculateAEW(startindex,endindex,63)
+    print("FOR N=64 and " + segment + " segment!")
+    print("AE: " + str(achievedEndurance))
+    print("AEW: " + str(aewMax64))
+    print("AE/AEW " + str(achievedEndurance/aewMax64))
+    print("AEW/AE " + str(aewMax64/achievedEndurance))
+    print("left and right " + str(leftindex64), str(rightindex64))
+    print("\n\n")
 
 # =====================================
 # ===== Plotting ======================
 # =====================================
 
-
 #======MemoryTracesPlot======
+
+#======WHOLE MEMORY TRACE====
+
 fig1, ax1 = plt.subplots()
 ax1.set_title("")
 ax1.set_xlabel("Adressess")
@@ -353,90 +395,99 @@ ax1.legend()
 ax1.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 ax1.xaxis.set_tick_params(which='both', labelbottom=True)
 ax1.yaxis.set_tick_params(which='both', labelbottom=True)
-plt.close()
-
-#=====AEW64Plot======
-fig2, ax2 = plt.subplots()
-ax2.set_title("")
-ax2.set_ylabel("Flips")
-ax2.set_xlabel("Address")
-ax2.spines["right"].set_visible(False)
-ax2.spines["top"].set_visible(False)
-ax2.grid(True, which="both", linestyle='--', axis="y")
-ax2.semilogy(accesses[start:endindex], '-r', label='Accesses')
-ax2.semilogy(bits[startindex:leftindex64], flips[startindex:leftindex64], 'c.', label='Flips')
-ax2.semilogy(bits[leftindex64:rightindex64], flips[leftindex64:rightindex64], 'go', label='FlipsAEW')
-ax2.semilogy(bits[rightindex64:endindex], flips[rightindex64:endindex], 'c.')
-ax2.legend(loc='upper right')
-ax2.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-plt.close()
 
 
-#=====AEW32Plot======
+#======AEW64Plot======
 
-fig2, ax2 = plt.subplots()
-ax2.set_title("")
-ax2.set_xlabel("Flips")
-ax2.set_ylabel("Address")
-ax2.spines["right"].set_visible(False)
-ax2.spines["top"].set_visible(False)
-ax2.grid(True, which="both", linestyle='--', axis="y")
-ax2.semilogy(accesses[start:endindex], '-r')
-ax2.semilogy(bits[startindex:leftindex32], flips[startindex:leftindex32], 'c.', label='Flips')
-ax2.semilogy(bits[leftindex32:rightindex32], flips[leftindex32:rightindex32], 'g*', label='FlipsAEW')
-ax2.semilogy(bits[rightindex32:endindex], flips[rightindex32:endindex], 'c.')
-ax2.legend()
-ax2.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-plt.close()
+if args.w == "64" or "all":
+
+    fig2, ax2 = plt.subplots()
+    ax2.set_title("")
+    ax2.set_ylabel("Flips")
+    ax2.set_xlabel("Address")
+    ax2.spines["right"].set_visible(False)
+    ax2.spines["top"].set_visible(False)
+    ax2.grid(True, which="both", linestyle='--', axis="y")
+    ax2.semilogy(accesses[start:endindex], '-r', label='Accesses')
+    ax2.semilogy(bits[startindex:leftindex64], flips[startindex:leftindex64], 'c.', label='Flips')
+    ax2.semilogy(bits[leftindex64:rightindex64], flips[leftindex64:rightindex64], 'go', label='FlipsAEW')
+    ax2.semilogy(bits[rightindex64:endindex], flips[rightindex64:endindex], 'c.')
+    ax2.legend(loc='upper right')
+    ax2.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
 
-#=====AEW16Plot======
 
-fig3, ax3 = plt.subplots()
-ax3.set_title("")
-ax3.set_xlabel("Flips")
-ax3.set_ylabel("Address")
-ax3.spines["right"].set_visible(False)
-ax3.spines["top"].set_visible(False)
-ax3.grid(True, which="both", linestyle='--', axis="y")
-ax3.semilogy(accesses[start:endindex], '-r')
-ax3.semilogy(bits[startindex:leftindex16], flips[startindex:leftindex16], 'c.', label='Flips')
-ax3.semilogy(bits[leftindex16:rightindex16], flips[leftindex16:rightindex16], 'g*', label='FlipsAEW')
-ax3.semilogy(bits[rightindex16:endindex], flips[rightindex16:endindex], 'c.')
-ax3.legend()
-ax3.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-plt.close()
+#======AEW32Plot======
 
-#=====AEW8Plot======
+elif args.w == "32":
 
-fi4, ax4 = plt.subplots()
-ax4.set_title("")
-ax4.set_xlabel("Flips")
-ax4.set_ylabel("Address")
-ax4.spines["right"].set_visible(False)
-ax4.spines["top"].set_visible(False)
-ax4.grid(True, which="both", linestyle='--', axis="y")
-ax4.semilogy(accesses[start:endindex], '-r')
-ax4.semilogy(bits[startindex:leftindex8], flips[startindex:leftindex8], 'c.', label='Flips')
-ax4.semilogy(bits[leftindex8:rightindex8], flips[leftindex8:rightindex8], 'go', label='FlipsAEW')
-ax4.semilogy(bits[rightindex8:endindex], flips[rightindex8:endindex], 'c.')
-ax4.legend()
-ax4.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-plt.close()
+    fig2, ax2 = plt.subplots()
+    ax2.set_title("")
+    ax2.set_xlabel("Flips")
+    ax2.set_ylabel("Address")
+    ax2.spines["right"].set_visible(False)
+    ax2.spines["top"].set_visible(False)
+    ax2.grid(True, which="both", linestyle='--', axis="y")
+    ax2.semilogy(accesses[start:endindex], '-r')
+    ax2.semilogy(bits[startindex:leftindex32], flips[startindex:leftindex32], 'c.', label='Flips')
+    ax2.semilogy(bits[leftindex32:rightindex32], flips[leftindex32:rightindex32], 'go', label='FlipsAEW')
+    ax2.semilogy(bits[rightindex32:endindex], flips[rightindex32:endindex], 'c.')
+    ax2.legend()
+    ax2.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
 
-# ax5.set_title("")
-# ax5.set_xlabel("Adressess")
-# ax5.set_ylabel("Flips")
-# ax5.spines["right"].set_visible(False)
-# ax5.spines["top"].set_visible(False)
-# ax5.grid(True, which="both", linestyle='--', axis="y")
-# ax5.semilogy(bits[start:end], accesses[start:end],
-#   5                 '-r', linewidth=0.3, label="#Accesses")
-# ax5.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
-# ax5.xaxis.set_tick_params(which='both', labelbottom=True)
-# ax5.yaxis.set_tick_params(which='both', labelbottom=True)
 
-# fig5, (ax5, ax1) = plt.subplots(1,2)
-# fig5.set_size_inches(11.5,4.5)
 
-plt.show()
+#======AEW16Plot======
+
+elif args.w == "16":
+
+    fig3, ax3 = plt.subplots()
+    ax3.set_title("")
+    ax3.set_xlabel("Flips")
+    ax3.set_ylabel("Address")
+    ax3.spines["right"].set_visible(False)
+    ax3.spines["top"].set_visible(False)
+    ax3.grid(True, which="both", linestyle='--', axis="y")
+    ax3.semilogy(accesses[start:endindex], '-r')
+    ax3.semilogy(bits[startindex:leftindex16], flips[startindex:leftindex16], 'c.', label='Flips')
+    ax3.semilogy(bits[leftindex16:rightindex16], flips[leftindex16:rightindex16], 'go', label='FlipsAEW')
+    ax3.semilogy(bits[rightindex16:endindex], flips[rightindex16:endindex], 'c.')
+    ax3.legend()
+    ax3.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+
+#======AEW8Plot======
+
+elif args.w == "8":
+
+    fi4, ax4 = plt.subplots()
+    ax4.set_title("")
+    ax4.set_xlabel("Flips")
+    ax4.set_ylabel("Address")
+    ax4.spines["right"].set_visible(False)
+    ax4.spines["top"].set_visible(False)
+    ax4.grid(True, which="both", linestyle='--', axis="y")
+    ax4.semilogy(accesses[start:endindex], '-r')
+    ax4.semilogy(bits[startindex:leftindex8], flips[startindex:leftindex8], 'c.', label='Flips')
+    ax4.semilogy(bits[leftindex8:rightindex8], flips[leftindex8:rightindex8], 'go', label='FlipsAEW')
+    ax4.semilogy(bits[rightindex8:endindex], flips[rightindex8:endindex], 'c.')
+    ax4.legend()
+    ax4.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+
+    # ax5.set_title("")
+    # ax5.set_xlabel("Adressess")
+    # ax5.set_ylabel("Flips")
+    # ax5.spines["right"].set_visible(False)
+    # ax5.spines["top"].set_visible(False)
+    # ax5.grid(True, which="both", linestyle='--', axis="y")
+    # ax5.semilogy(bits[start:end], accesses[start:end],
+    #   5                 '-r', linewidth=0.3, label="#Accesses")
+    # ax5.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    # ax5.xaxis.set_tick_params(which='both', labelbottom=True)
+    # ax5.yaxis.set_tick_params(which='both', labelbottom=True)
+
+    # fig5, (ax5, ax1) = plt.subplots(1,2)
+    # fig5.set_size_inches(11.5,4.5)
+
+if args.p:
+    plt.show()
